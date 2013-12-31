995b6ecaf078ebccb10ff1394174935b9010f355
Choongmin Lee <choongmin@me.com>
2013-12-31 09:30:32 +0900

    refactor: remove obsolete comment

--- app/scripts/directives/progressBar.js
+++ app/scripts/directives/progressBar.js
@@ -2,7 +2,6 @@
 
 angular.module('doshi')
   .directive('progressBar', function () {
-    // TODO inspect why it doesn't work in IE
     return {
       scope: {value: '=model'},
       templateUrl: 'templates/progress-bar.html'
