4e22558d652df099e39f247ddcbc6e3950c28493
Choongmin Lee <choongmin@me.com>
2013-12-31 10:55:55 +0900

    refactor(output)

--- app/scripts/controllers/output.js
+++ app/scripts/controllers/output.js
@@ -8,7 +8,6 @@ angular.module('doshi')
     $scope.status = appService.status;
     $scope.timetable = appService.timetable;
     $scope.timetableStats = appService.timetableStats;
-    $scope.isHighlighted = [];
     $scope.highlightedCourse = undefined;
     $scope.highlightedClass = undefined;
 
@@ -24,55 +23,47 @@ angular.module('doshi')
       appService.resume();
     };
 
-    $scope.highlightCourse = function (course) {
-      $scope.highlightedCourse = course;
-      $scope.highlightedClass = undefined;
+    function forEachAssignment(callback) {
       for (var i = 0; i < appService.numDays; i++) {
         for (var j = 0; j < appService.numPeriods; j++) {
-          $scope.isHighlighted[i][j] = [];
           var assignments = appService.timetable[i][j];
           if (!assignments) continue;
           for (var k = 0; k < assignments.length; k++) {
-            if (assignments[k][0] === course) {
-              $scope.isHighlighted[i][j][k] = true;
-              break;
-            }
+            callback.call(undefined, assignments[k]);
           }
         }
       }
+    }
+
+    $scope.highlightCourse = function (course) {
+      $scope.highlightedCourse = course;
+      $scope.highlightedClass = undefined;
+      forEachAssignment(function (assignment) {
+        assignment.isHighlighted = assignment[0] === course;
+      });
     };
 
     $scope.highlightClass = function (klass) {
       $scope.highlightedCourse = undefined;
       $scope.highlightedClass = klass;
-      for (var i = 0; i < appService.numDays; i++) {
-        for (var j = 0; j < appService.numPeriods; j++) {
-          $scope.isHighlighted[i][j] = [];
-          var assignments = appService.timetable[i][j];
-          if (!assignments) continue;
-          for (var k = 0; k < assignments.length; k++) {
-            var classGroup = assignments[k][1];
-            for (var l = 0; l < classGroup.length; l++) {
-              if (classGroup[l] === klass) {
-                $scope.isHighlighted[i][j][k] = true;
-                break;
-              }
-            }
-            if ($scope.isHighlighted[i][j][k]) break;
+      forEachAssignment(function (assignment) {
+        assignment.isHighlighted = false;
+        var classGroup = assignment[1];
+        for (var l = 0; l < classGroup.length; l++) {
+          if (classGroup[l] === klass) {
+            assignment.isHighlighted = true;
+            break;
           }
         }
-      }
+      });
     };
 
     function dehighlight() {
       $scope.highlightedCourse = undefined;
       $scope.highlightedClass = undefined;
-      for (var i = 0; i < appService.numDays; i++) {
-        $scope.isHighlighted[i] = [];
-        for (var j = 0; j < appService.numPeriods; j++) {
-          $scope.isHighlighted[i][j] = [];
-        }
-      }
+      forEachAssignment(function (assignment) {
+        assignment.isHighlighted = false;
+      });
     }
 
     screenclick(function () { $scope.$apply(dehighlight); });
